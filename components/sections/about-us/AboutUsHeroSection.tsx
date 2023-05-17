d6a53f0724efd73590c0b9c288253e1bc1e63754
Fernando G <fernanprojects@gmail.com>
2023-05-17 17:27:39 +0200

    Refactorized AboutUsHeroSection

--- components/sections/about-us/AboutUsHeroSection.tsx
+++ components/sections/about-us/AboutUsHeroSection.tsx
@@ -4,48 +4,58 @@ import { PainPoint } from "../home-page/bottom-section/PainPoint";
 import { PainPointIconTwo } from "@/components/icons/PainPointIconTwo";
 import { PainPointIconOne } from "@/components/icons/PainPointIconOne";
 import { PainPointIconThree } from "@/components/icons/PainPointIconThree";
+import { InfoHeroSection } from "../hero-section/InfoHeroSection";
+import { ReactElement } from "react";
+
+type InfoHeroSectionProps = Parameters<typeof InfoHeroSection>[number];
 
 export const AboutUsHeroSection = () => {
+  const headerSection: Record<keyof InfoHeroSectionProps, ReactElement> = {
+    header: (
+      <div>
+        <h1 className={styles.aboutUsHero_title}>
+          Nacemos de la{" "}
+          <span className={styles.aboutUsHero_title_highlight}>innovación</span>
+        </h1>
+        <p className={styles.aboutUsHero_subtitle}>
+          Con más de 25 años de experiencia, <br />
+          revolucionamos la inversión inmobiliaria para hacerla accesible a
+          partir de 500€.
+        </p>
+      </div>
+    ),
+    elementOne: (
+      <PainPoint
+        title="Transparencia"
+        text="Nuestra claridad y honestidad para priorizar a quienes confían en nosotros"
+        icon={<PainPointIconOne />}
+      />
+    ),
+    elementTwo: (
+      <PainPoint
+        title="Seguridad"
+        text="La confianza depositada en nosotros requiere la máxima implicación y tenacidad para cumplir los objetivos."
+        icon={<PainPointIconTwo />}
+      />
+    ),
+    elementThree: (
+      <PainPoint
+        title="Evolución"
+        text="La constante mejora para alcanzar nuevas metas para ser cada vez mejores."
+        icon={<PainPointIconThree />}
+      />
+    ),
+  };
+
   return (
     <SectionLayout>
       <div className={styles.aboutUsHero}>
-        <div>
-          <div>
-            <h1 className={styles.aboutUsHero_title}>
-              Nacemos de la{" "}
-              <span className={styles.aboutUsHero_title_highlight}>
-                innovación
-              </span>
-            </h1>
-            <p className={styles.aboutUsHero_subtitle}>
-              Con más de 25 años de experiencia, <br />
-              revolucionamos la inversión inmobiliaria para hacerla accesible a
-              partir de 500€.
-            </p>
-          </div>
-
-          <div className={styles.aboutUsHero_painPointsWrapper}>
-            <div className={styles.aboutUsHero_painPointContent}>
-              <PainPoint
-                title="Transparencia"
-                text="Nuestra claridad y honestidad para priorizar a quienes confían en nosotros"
-                icon={<PainPointIconOne />}
-              />
-
-              <PainPoint
-                title="Seguridad"
-                text="La confianza depositada en nosotros requiere la máxima implicación y tenacidad para cumplir los objetivos."
-                icon={<PainPointIconTwo />}
-              />
-
-              <PainPoint
-                title="Evolución"
-                text="La constante mejora para alcanzar nuevas metas para ser cada vez mejores."
-                icon={<PainPointIconThree />}
-              />
-            </div>
-          </div>
-        </div>
+        <InfoHeroSection
+          header={headerSection.header}
+          elementOne={headerSection.elementOne}
+          elementTwo={headerSection.elementTwo}
+          elementThree={headerSection.elementThree}
+        />
       </div>
     </SectionLayout>
   );
