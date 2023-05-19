b8d17ef19cee87739a99a9c66af2a265965ba229
Fernando C4F <fernando@crowd4flipping.com>
2023-05-19 12:43:12 +0200

    Moved Open Graph meta tags to the _app page

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,45 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Head>
+        <title>Crowd4Flipping</title>
+        <meta
+          name="description"
+          content="La inversión inmobiliaria al alcance de todos."
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+        {/* FB META TAGS */}
+        <meta property="og:url" content="https://www.crowd4flipping.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Crowd4Flipping" />
+        <meta
+          property="og:description"
+          content="La inversión inmobiliaria al alcance de todos."
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/crowd4flipping-app.appspot.com/o/opengraph%2Fopengraph-c4f.png?alt=media&token=a880e75c-80c0-4820-80f4-284b47685cc8"
+        />
+
+        {/* TWITTER META TAGS */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="crowd4flipping.com" />
+        <meta
+          property="twitter:url"
+          content="https://www.crowd4flipping.com/"
+        />
+        <meta name="twitter:title" content="Crowd4Flipping" />
+        <meta
+          name="twitter:description"
+          content="La inversión inmobiliaria al alcance de todos."
+        />
+        <meta
+          name="twitter:image"
+          content="https://firebasestorage.googleapis.com/v0/b/crowd4flipping-app.appspot.com/o/opengraph%2Fopengraph-c4f.png?alt=media&token=a880e75c-80c0-4820-80f4-284b47685cc8"
+        />
+      </Head>
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
