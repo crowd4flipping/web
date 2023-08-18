66946defe87133387beb76ee21092dc88be83614
Fernando G <fernanprojects@gmail.com>
2023-08-18 13:16:10 +0200

    Add how it works point one

--- components/sections/pro/ProHowItWorks.tsx
+++ components/sections/pro/ProHowItWorks.tsx
@@ -1,3 +1,4 @@
+import { PainPointIconOne } from "@/components/icons/PainPointIconOne";
 import { DashedLine } from "./DasedLine";
 import styles from "./styles/ProHowItWorks.module.scss";
 
@@ -8,9 +9,23 @@ export const ProHowItWorks = () => {
         ¿Cómo funciona Crowd4Flipping PRO?
       </h2>
 
-      <div className={styles.howItWorks__dashed_content}>
-        <div className={styles.howItWorks__dashed_line_wrapper}>
-          <DashedLine rounded="br" />h
+      <div className={styles.howItWorks__content}>
+        <div className={styles.howItWorks__point_one}>
+          <div style={{ width: "2rem", height: "2rem" }}>
+            <PainPointIconOne />
+          </div>
+          <h3 className={styles.howItWorks__point_one_title}>
+            ANÁLISIS DE MERCADO
+          </h3>
+          <p>
+            Estudiamos tendencias y datos para identificar áreas prometedoras en
+            el mercado inmobiliario.
+          </p>
+        </div>
+        <div className={styles.howItWorks__dashed_content}>
+          <div className={styles.howItWorks__dashed_line_wrapper}>
+            <DashedLine rounded="br" />
+          </div>
         </div>
       </div>
     </section>
