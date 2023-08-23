037b18f136d7da0d64166298e764303a364c6780
Fernando G <fernanprojects@gmail.com>
2023-08-23 12:41:08 +0200

    Add Rive Animation

--- components/sections/pro/ProHeroSection.tsx
+++ components/sections/pro/ProHeroSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { ProHeroHeader } from "./ProHeroHeader";
 import styles from "./styles/ProHeroSection.module.scss";
-import C4FProLogo from "@/public/images/brand/crowd4flipping/logo/c4f_pro.svg";
+import C4FLogo from "@/public/images/brand/crowd4flipping/logo/Crowd4Flipping.svg";
 
 export const ProHeroSection = () => {
   return (
@@ -11,7 +11,7 @@ export const ProHeroSection = () => {
         <div className={styles.proHeroSection__nav_and_hero}>
           <nav className={styles.proHeroSection__navbar}>
             <Image
-              src={C4FProLogo}
+              src={C4FLogo}
               alt="C4FPro"
               width={100}
               height={100}
@@ -20,18 +20,21 @@ export const ProHeroSection = () => {
           </nav>
 
           <section className={styles.proHeroSection__hero}>
-            
             <div className={styles.proHeroSection__hero_content}>
               <ProHeroHeader />
             </div>
 
             <div
-              style={{
-                backgroundColor: "rgb(128 0 128 / 30%)",
-                height: "100%",
-                width: "100%",
-              }}
-            ></div>
+              className={styles.proHeroSection__hero_animation_wrapper}
+            >
+              <iframe
+                className={styles.proHeroSection__hero_animation}
+                width="100%"
+                height="100%"
+                src="https://rive.app/s/smnUpcGs3ESdnK7l-ulNPQ/embed"
+                allowFullScreen
+              ></iframe>
+            </div>
           </section>
         </div>
       </div>
