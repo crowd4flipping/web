0c0bd419d62245a6afe1bf4bb13e952bf6cec1f2
Fernando G <fernanprojects@gmail.com>
2023-08-23 11:29:29 +0200

    Refact Hero section ProTag

--- components/sections/pro/ProTag.tsx
+++ components/sections/pro/ProTag.tsx
@@ -3,10 +3,10 @@ import styles from "./styles/ProTag.module.scss";
 export const ProTag = () => {
   return (
     <div className={styles.proTag}>
-      <div className={styles.proTag__container}>
-        <div className={styles.proTag__container_content} />
+      <div className={styles.proTag__container}></div>
+      <div className={styles.proTag__container_content}>
+        <h2 className={styles.proTag__text}>PRO</h2>
       </div>
-      <h2 className={styles.proTag__text}>PRO</h2>
     </div>
   );
 };
