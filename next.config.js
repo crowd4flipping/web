9d3779192c1217834608113b820381fda2d6acd6
Fernando G <fernanprojects@gmail.com>
2023-08-25 10:49:34 +0200

    Add next config rewrites

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    domains: ["firebasestorage.googleapis.com"],
   },
-}
+  rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: "/pro",
+          has: [
+            {
+              type: "host",
+              value: "pro.crowd4flipping.com",
+            },
+          ],
+          destination: "/pro",
+        },
+      ],
+    };
+  },
+};
