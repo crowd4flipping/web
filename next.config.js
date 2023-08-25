e898609b780a904e251321043b3abf69bbb3fd83
Fernando G <fernanprojects@gmail.com>
2023-08-25 14:15:35 +0200

    Remov beforFiles in next config

--- next.config.js
+++ next.config.js
@@ -2,23 +2,22 @@ module.exports = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  assetPrefix: 'crowd4flipping.com',
+  assetPrefix: "crowd4flipping.com",
   rewrites() {
-    return {
-      beforeFiles: [
-        // if the host is `app.acme.com`,
-        // this rewrite will be applied
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "pro.crowd4flipping.com",
-            },
-          ],
-          destination: "/pro",
-        },
-      ],
-    };
+    return;
+    /* {
+      beforeFiles: */ [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "pro.crowd4flipping.com",
+          },
+        ],
+        destination: "/pro",
+      },
+    ];
+    /*  }; */
   },
 };
