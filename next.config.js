6425c816a1979e40e1299bed4dcbd8adbd58005f
Fernando G <fernanprojects@gmail.com>
2023-08-25 14:17:19 +0200

    Changes

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@ module.exports = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  assetPrefix: "crowd4flipping.com",
+  assetPrefix: "https://crowd4flipping.com",
   rewrites() {
-    return;
-    /* {
-      beforeFiles: */ [
+    return [
       {
         source: "/:path*",
         has: [
@@ -18,6 +16,23 @@ module.exports = {
         destination: "/pro",
       },
     ];
-    /*  }; */
   },
+  /* rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "pro.crowd4flipping.com",
+            },
+          ],
+          destination: "/pro",
+        },
+      ],
+    };
+  }, */
 };
