5c0a51b24ef51017311e748a95b46911f7c26105
Fernando G <fernanprojects@gmail.com>
2023-08-25 11:36:01 +0200

    Create vercel.json rewrites

--- next.config.js
+++ next.config.js
@@ -2,22 +2,4 @@ module.exports = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  rewrites() {
-    return {
-      beforeFiles: [
-        // if the host is `app.acme.com`,
-        // this rewrite will be applied
-        {
-          source: "/pro",
-          has: [
-            {
-              type: "host",
-              value: "pro.crowd4flipping.com",
-            },
-          ],
-          destination: "/pro",
-        },
-      ],
-    };
-  },
 };
