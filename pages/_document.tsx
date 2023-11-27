3abe68156e23c8660ca1976ebc0da1c6485ee500
Fernando G <fernanprojects@gmail.com>
2023-11-27 20:29:11 +0100

    Comment header scripts and google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,10 +24,10 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
+          {/* <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600;700&family=Urbanist:ital,wght@0,100;0,400;0,700;0,900;1,100;1,400;1,700;1,900"
-          />
+          /> */}
         </Head>
         <body>
           <noscript>
